a4da46a665e66bdacdc933f96fda8d2052a392a2
Niamat Marjan <86093827+NimBuzz01@users.noreply.github.com>
2022-08-23 18:58:27 +0530

    added validation script

--- scripts/validation.js
+++ scripts/validation.js
@@ -3,7 +3,7 @@ function validation(nic) {
     var result = false;
     if (nic.length === 10 && !isNaN(nic.substr(0, 9)) && isNaN(nic.substr(9, 1).toLowerCase()) && ['x', 'v'].includes(nic.substr(9, 1).toLowerCase())) {
         result = true;
-    } else if (nicNumber.length === 12 && !isNaN(nicNumber)) {
+    } else if (nic.length === 12 && !isNaN(nic)) {
         result = true;
     } else {
         result = false;
