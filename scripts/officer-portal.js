6268e86f152fca2ee7d35ed0aa309579ada077ff
AbdulHakkam <abdulhakkamazeez@gmail.com>
2022-08-28 14:08:44 +0530

    auth

--- scripts/officer-portal.js
+++ scripts/officer-portal.js
@@ -169,18 +169,23 @@ function createRequests(arr,token,gramaId){
           console.log(" no summary")
 
         }else{
+          console.log("1");
           records=summary.split(",");
           var list = document.createElement("ul");
+          console.log("2");
           for(var i=0;i<records.length;i++){
+            console.log("3");
             var info = records[i];
             info = info.replace("{","");
             info = info.replace("}","");
             info = info.replace(")","");
             info = info.replace("(",",");
             moreInfo = info.split(",");
+            console.log("4");
             info[0] = info[0].bold();
             var record = document.createElement("li");
             record.appendChild(document.createTextNode(info.join(":")));
+            console.log("5");
             list.append(record);
             console.log(info);
 
