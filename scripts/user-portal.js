433c2e7998eddaafb54ef4b3d1c7bd1d80380861
Kumuditha Dodanwela <86454991+KD23243@users.noreply.github.com>
2022-08-31 10:20:48 +0530

    Update user-portal.js

--- scripts/user-portal.js
+++ scripts/user-portal.js
@@ -1,5 +1,5 @@
-// gramaURL = "https://production-50qdi0pj.wl.gateway.dev/";
-gramaURL = "https://grama-app-backend-dot-choreo-asgardeo-intern-project.de.r.appspot.com/";
+gramaURL = "https://production-grama-app-88dwvjly.wl.gateway.dev";
+// gramaURL = "https://grama-app-backend-dot-choreo-asgardeo-intern-project.de.r.appspot.com/";
 
 // Sweet Alerts
 const preLoadAlert = () => {
